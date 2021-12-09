17c86e4cd1b4bb99f54cf9351c469763851ca0a8
Vemurigp <Vemurigp@DESKTOP-5OUSQF8>
2021-12-09 13:51:07 +0530

    Test Demo Added

---
+++ src/test/java/com/crm/SDET26practice/TestPractice/Demo.java
@@ -0,0 +1,12 @@
+package com.crm.SDET26practice.TestPractice;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+@Test
+public void demoProgram()
+{
+System.out.println("this is uesr --->1,  SCRIPT --->1 ");	
+
+}
+}
